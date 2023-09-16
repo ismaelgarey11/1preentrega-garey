f31f10146a879198c36f40f0b051ad3616ad5f78
ismael <ismaelgarey11@gmail.com>
2023-09-16 16:43:37 -0300

    nueva preentrega

--- javascript.js
+++ javascript.js
@@ -1,40 +1,37 @@
-//condicionales
-let Nombre = prompt(`ingrese su nombre`)
-let Apellido = prompt(`ingrese su apellido`)
-if ((Nombre == "") && ( Apellido == "")){
-    alert("no ingresaste nombre de usuario");
-}
-else {
-alert(`bienvenido a vinland ${Nombre} ${Apellido}`)
-}
-alert("hay mangas con hasta un 15% de descuento")
-let titulo =parseInt(prompt("ingrese el valor del libro"))
-if (titulo >= 15000)
-{alert(`el precio de su libro obtiene un 15% de descuento`)}
-else (titulo <= 15000); {alert("lamentablemente no posee descuento")}
-
-const productos = [
-  { nombre: "mangas", precio: 10 },
-  { nombre: "comics", precio: 20 },
-  { nombre: "figuras", precio: 5 },
-];
-
-
-let total = 0;
-
-
-for (let i = 0; i < productos.length; i++) {
-  
-  const cantidad = parseInt(prompt(`¿Cuántos "${productos[i].nombre}" desea comprar?`));
-  
-  
-  if (!isNaN(cantidad) && cantidad >= 0) {
-    total += productos[i].precio * cantidad;
-  } else {
-    alert("La cantidad ingresada no es válida. Debe ser un número mayor o igual a cero.");
-
-    i--;
-  }
+// Carrito de compras
+const carrito = [];
+        
+// Función para agregar productos al carrito
+function agregarAlCarrito(nombre, precio) {
+    carrito.push({ nombre, precio });
+    actualizarCarrito();
 }
 
-console.log(`El total de la compra es: $${total}`);
+// Función para actualizar el contenido del carrito y calcular el total
+function actualizarCarrito() {
+    const carritoLista = document.getElementById('carrito-lista');
+    const totalElement = document.getElementById('total');
+    let total = 0;
+
+    carritoLista.innerHTML = '';
+
+    carrito.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${item.nombre} - $${item.precio}`;
+        carritoLista.appendChild(listItem);
+        total += item.precio;
+    });
+
+    totalElement.textContent = total;
+}
+
+// Función para aplicar un descuento al total
+function aplicarDescuento(descuentoPorcentaje) {
+    const totalElement = document.getElementById('total');
+    let total = parseFloat(totalElement.textContent);
+    if (!isNaN(total)) {
+        const descuento = (total * descuentoPorcentaje) / 100;
+        total -= descuento;
+        totalElement.textContent = total.toFixed(2);
+    }
+}
